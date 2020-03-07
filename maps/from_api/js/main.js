2a42484349813e6469486aa6567da22cf80f7bf1
Jose Juan García <dante.arkade7@gmail.com>
2020-03-07 15:49:26 -0600

    Version 0.1

--- maps/from_api/js/main.js
+++ maps/from_api/js/main.js
@@ -2,9 +2,15 @@
 
 let map = L.map('map').setView([19.432545, -99.133209], 12);
 
+L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    id: 'MapID',
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>'
+}).addTo(map);
+
 let url = 'https://datos.cdmx.gob.mx/api/records/1.0/search/';
 
 let controls = L.control.groupedLayers().addTo(map);
+let circle_filter = L.layerGroup().addTo(map);
 
 let color_by_route = {
     "CORREDORES CONCESIONADOS": '#FF8D80', 
@@ -16,87 +22,150 @@ let color_by_route = {
     "TREN LIGERO":'#9CA2FF'
 }
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    id: 'MapID',
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>'
-}).addTo(map);
 
+
+
+const create_gtfs_layers = (data) => {
+    gtfs_layers(data[0]);
+    cetram(data[1]);
+}
+
+const create_bici_layers = (data) => {
+    ecobici(data[0]);
+    sitis(data[1]);
+}
+
+let gtfs_req = new Requests({
+    requests_elements: [
+        {
+            url: url,
+            urldata: {
+                dataset: 'gtfs-general-transit-feed-specifications-actualizado-de-la-ciudad-de-mexico',
+                rows: 500
+            }
+        },
+        {
+            url: url,
+            urldata: {
+                dataset: 'ubicacion-de-centros-de-transferencia-modal-cetram',
+                rows: 50
+            }
+        }
+
+    ],
+    then_callback: create_gtfs_layers
+});
+
+const query_bici = (lat, lng, meters) => {
+    let bici_req = new Requests({
+        requests_elements: [
+            {
+                url: url,
+                urldata: {
+                    dataset: 'estaciones-de-ecobici',
+                    rows: 500,
+                    'geofilter.distance': `${lat},${lng},${meters}`
+                }
+            },
+            {
+                url: url,
+                urldata: {
+                    dataset: 'puntos-de-arribo-sitis',
+                    rows: 75,
+                    'geofilter.distance': `${lat},${lng},${meters}`
+                }
+            },
+
+        ],
+        then_callback: create_bici_layers
+    })
+    bici_req.send()
+}
 
 const gtfs_layers = (data) => {
-    let records = data[0].records,
+    let records = data.records,
         group_layers = {};
     for (let item of records) {
         if (!(item.fields.agencia in group_layers)) {
             group_layers[item.fields.agencia] = L.layerGroup().addTo(map);
             controls.addOverlay(group_layers[item.fields.agencia], item.fields.agencia, 'GTFS');
         }
-        // L.geoJSON(item.geometry).addTo(group_layers[item.fields.agencia]);
         L.geoJSON(item.fields.geo_shape, {
-            style: function (feature) {
-                return { color: color_by_route[item.fields.agencia] };
-            }
+            style: () => { return { color: color_by_route[item.fields.agencia] } }
         }).addTo(group_layers[item.fields.agencia]);
-        
+
     }
-   
+
 }
 
 const ecobici = (data) => {
-    let records = data[0].records;
-    let ecobici_lg = L.layerGroup().addTo(map);
-    controls.addOverlay(ecobici_lg, 'ecobici', 'ecobici');
+    let records = data.records;
+    var bici_icon = L.AwesomeMarkers.icon({
+        prefix: 'fa',
+        icon: 'bicycle',
+        markerColor: 'red'
+    });
+    // controls.addOverlay(ecobici_lg, 'ecobici', 'ecobici');
     for (let item of records) {
-        L.geoJSON(item.geometry).addTo(ecobici_lg);
+        let ll = L.latLng(item.fields.punto_geo);
+        L.marker(ll, { icon: bici_icon }).addTo(circle_filter)
+        // L.geoJSON(item.geometry).addTo(circle_filter);
     }
 } 
 
-const cetram = (data) => {
-    let records = data[0].records;
-    let ecobici_lg = L.layerGroup().addTo(map);
-    controls.addOverlay(ecobici_lg, 'cetram', 'cetram');
+const sitis = (data) => {
+    let records = data.records;
+    var sitis_icon = L.AwesomeMarkers.icon({
+        prefix: 'fa',
+        icon: 'map-marker',
+        markerColor: 'green'
+    });
+    // controls.addOverlay(sitis_lg, 'sitis', 'sitis');
     for (let item of records) {
-        L.geoJSON(item.geometry).addTo(ecobici_lg);
+        // L.geoJSON(item.geometry).addTo(circle_filter);
+        let ll = L.latLng(item.fields.punto_geo);
+        L.marker(ll, { icon: sitis_icon }).addTo(circle_filter)
+    }
+}
+
+const cetram = (data) => {
+    let records = data.records;
+    let cetram_lg = L.layerGroup().addTo(map);
+    var cetram_icon = L.AwesomeMarkers.icon({
+        prefix: 'fa',
+        icon: 'exchange',
+        markerColor: 'orange'
+    });
+    controls.addOverlay(cetram_lg, 'cetram', 'cetram');
+    for (let item of records) {
+        // console.log(item);
+        let html_popup = `
+            <div>Ubicación: ${item.fields.ubicacion}</div>
+            <div>Metrobus: ${item.fields.t_metrobus}</div>
+            <div>Metro: ${item.fields.t_metro}</div>            
+        `
+        let ll = L.latLng(item.fields.geo_point_2d);
+        let marker = L.marker(ll, { icon: cetram_icon }).bindPopup(html_popup).addTo(cetram_lg);
+
+        // let marker = L.geoJSON(item.geometry).bindPopup( () => {
+        //     return html_popup;
+        // }).addTo(cetram_lg);
+        marker.on('click', (e) => {
+            let circle_layers = circle_filter.getLayers();
+            if (circle_layers.length > 0) {
+                circle_filter.clearLayers();
+                map.setView([19.432545, -99.133209], 12);
+                return
+            }
+            L.circle(e.latlng, 500).addTo(circle_filter);
+            map.setView(e.latlng, 15);
+            query_bici(e.latlng.lat, e.latlng.lng, 500);
+        })
     }
 } 
 
 const init = () => {
 
-    let gtfs_req = new Requests({
-        then_callback: gtfs_layers
-    });
-    gtfs_req.appendRequest({
-        url: url,
-        urldata: {
-            dataset: 'gtfs-general-transit-feed-specifications-actualizado-de-la-ciudad-de-mexico',
-            rows: 500
-        }
-    });
     gtfs_req.send()
-
-    let ecobici_req = new Requests({
-        then_callback: ecobici
-    });
-
-    ecobici_req.appendRequest({
-        url: url,
-        urldata: {
-            dataset: 'estaciones-de-ecobici',
-            rows: 500
-        }
-    });
-    ecobici_req.send()
-
-    let cetram_req = new Requests({
-        then_callback: cetram
-    });
-
-    cetram_req.appendRequest({
-        url: url,
-        urldata: {
-            dataset: 'ubicacion-de-centros-de-transferencia-modal-cetram',
-            rows: 50
-        }
-    });
-    cetram_req.send()
 
 }
