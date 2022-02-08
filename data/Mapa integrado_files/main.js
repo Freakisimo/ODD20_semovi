bc38c33838e9b3352deb672f693eedc75a8b3f76
dante7 <dante.arkade7@gmail.com>
2022-02-08 01:16:16 -0600

    Change html and add firestore to consume al the data

--- data/Mapa integrado_files/main.js
+++
@@ -1,8 +0,0 @@
-"use strict;"
-
-const init = () => {
-    let map = L.map('map').setView([19.432545, -99.133209], 18);
-
-    L.tileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png',
-    {id: 'MapID', attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>'}).addTo(map);
-}
---
+++ js/config.js
@@ -0,0 +1,9 @@
+"use strict;"
+const firebaseConfig = {
+  apiKey: "AIzaSyCuwQ5KG-loDTikaGkYj4xzVGUDL0ePCyk",
+  authDomain: "public-transport-cdmx.firebaseapp.com",
+  projectId: "public-transport-cdmx",
+  storageBucket: "public-transport-cdmx.appspot.com",
+  messagingSenderId: "431072255571",
+  appId: "1:431072255571:web:d1fe7676662a452cc830a6"
+};
